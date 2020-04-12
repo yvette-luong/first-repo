5db1f05fd908af0bccfc44f239d1d672122d2fb4
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-04-11 22:24:25 -0700

    my first lesson

---
+++ data-types.py
@@ -0,0 +1,38 @@
+"""
+Boolean = True or False
+String = "Yvette"
+Undefined = A value that is not defined
+Integer = 132432
+Camel Case = lower case first word and then capitalize each following word:
+    example: helloWorld
+"""
+ 
+"1231314"
+
+Yvette = 123
+
+Yvette
+
+def helloYvette(): # this is a function, which is indicated by brackets
+    print("Hello my name is Yvette")
+
+helloYvette() # calling the function
+
+def helloAnyone(name): # accept a parameter
+    print(f"Hello my name is {name}")
+
+helloAnyone("sjfajkhf") # sjfajkhf is an argument
+
+def multiply(x, y):
+    return x * y
+
+print(multiply(6, 2))
+
+print("Hello this is print")
+
+
+def add(a, b):
+    return a + b 
+
+print(add(4, 5))
+
